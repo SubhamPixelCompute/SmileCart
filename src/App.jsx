846d7444d6d19d713f5be54a996a2a43724e179f
Subham15112001 <sinchan889377888@gmail.com>
2025-06-12 11:10:09 +0530

    Added product listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Switch } from "react-router-dom";
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { NavLink, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
-import Home from "./components/Home";
-import PageNotFound from "./components/PageNotFound";
+import PageNotFound from "./components/commons/PageNotFound";
 import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
 const App = () => (
   <>
@@ -16,8 +16,9 @@ const App = () => (
       </NavLink>
     </div>
     <Switch>
-      <Route exact component={Home} path="/" />
-      <Route exact component={Product} path="/product" />
+      <Route exact component={ProductList} path="/products" />
+      <Route exact component={Product} path="/products/:slug" />
+      <Redirect exact from="/" to="/products" />
       <Route component={PageNotFound} path="*" />
     </Switch>
   </>
